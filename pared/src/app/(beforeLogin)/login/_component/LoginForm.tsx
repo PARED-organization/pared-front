013cf249d8baa1fd79bce9377e4a4858f03fa98c
sunmi An <aseonmi220@gmail.com>
2025-07-15 23:55:36 +0900

    feat: 로그인페이지 ui 완료

--- pared/src/app/(beforeLogin)/login/_component/LoginForm.tsx
+++ pared/src/app/(beforeLogin)/login/_component/LoginForm.tsx
@@ -1,7 +1,24 @@
 export default function LoginForm() {
-    return (
-        <>
-        로그인 폼
-        </>
-    )
+  return (
+    <>
+      <form className="flex flex-col gap-[10px] mt-[50px]">
+        <input
+          className="bg-[#fff] w-[420px] h-[44px] border border-[#D9D9D9] rounded-[4px] pl-[25px] focus:outline-none font-medium text-[15px]"
+          type="text"
+          placeholder="이메일"
+        />
+        <input
+          className="bg-[#fff] w-[420px] h-[44px] border border-[#D9D9D9] rounded-[4px] pl-[25px] focus:outline-none font-medium text-[15px]"
+          type="password"
+          placeholder="비밀번호"
+        />
+        <button className="w-[420px] h-[44px] bg-[#FF9466] text-[#fff] rounded-[4px] cursor-pointer font-medium text-[15px]">
+          로그인
+        </button>
+        <button className="bg-[#fff] w-[420px] h-[44px] border border-[#D9D9D9] text-[#222222] rounded-[4px] cursor-pointer font-medium text-[15px]">
+          회원가입
+        </button>
+      </form>
+    </>
+  );
 }
--- pared/src/app/(beforeLogin)/login/page.tsx
+++ pared/src/app/(beforeLogin)/login/page.tsx
@@ -3,18 +3,31 @@ import LoginForm from "./_component/LoginForm";
 import SocialLogin from "./_component/SocialLogin";
 
 export default function LoginPage() {
-    return (
-        <>
-        <div className="flex w-full h-screen">
-            <div className="w-1/2 h-full relative">
-                <Image src="/images/auth/auth_bg.png" alt="login_bg" fill />
-            </div>
-            <div className="w-1/2 h-full flex flex-col justify-center items-center">
-                <Image src="/images/common/logo.png" alt="logo" width={268} height={91} />
-                <LoginForm />
-                <SocialLogin />
-            </div>
+  return (
+    <>
+      <div className="flex w-full h-screen">
+        <div className="w-1/2 h-full relative">
+          <Image src="/images/Auth/auth_bg.png" alt="login_bg" fill />
         </div>
-        </>
-    )
+        {/* background image */}
+        <div className="relative w-1/2 h-full flex flex-col justify-center items-center overflow-hidden">
+          <div
+            className="absolute top-0 left-0 right-0 bottom-0 w-full h-full bg-contain bg-center bg-no-repeat z-0"
+            style={{ backgroundImage: "url('/images/common/gradient_bg.png')" }}
+          />
+          {/* content */}
+          <div className="relative z-10 flex flex-col items-center">
+            <Image
+              src="/images/common/logo.png"
+              alt="logo"
+              width={268}
+              height={91}
+            />
+            <LoginForm />
+            <SocialLogin />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
